3a486365fe765168a5f15c6deea19f726df0586d
Khaled Talaat El-Zeiny <68670333+Khaled0Talaat@users.noreply.github.com>
2020-10-13 23:33:40 +0100

    Add files via upload

---
+++ 03-MCAL/03-STK/STK_config.h
@@ -0,0 +1,16 @@
+/*****************************************/
+/*         SYSTick Driver                */
+/*                                       */
+/* Author  :  Khaled Talaat El-Zeiny     */
+/* Version :  V01                        */
+/* Date    :  26 August 2020             */
+/*****************************************/
+#ifndef STK_CONFIG_H
+#define STK_CONFIG_H
+
+
+/* Options: MSTK_SRC_AHB / MSTK_SRC_AHB_8 */
+#define MSTK_CLK_SRC     MSTK_SRC_AHB_8
+
+
+#endif
